28a130955cb69fe8c34312a4b727937518a58f8b
Suhail Nizami <63776757+suhailnizami01jan@users.noreply.github.com>
2023-06-06 15:11:49 +0530

    Add files via upload

---
+++ UnitTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestAutomation
+{
+
+	[TestClass]
+	public class UnitTest
+	{
+
+       [TestMethod]
+       public void Testmethod5()
+       {
+
+       }
+
+       [TestMethod]
+       public void Testmethod4()
+       {
+
+       }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestAutomation
+{
+    [TestClass]
+	public UnitTest1
+	{
+       
+	   [TestMethod]
+       public void Testmethod1()
+       {
+
+       }
+
+       [TestMethod]
+       public void Testmethod2()
+       {
+
+       }
+
+       [TestMethod]
+       public void Testmethod3()
+       {
+         
+       }
+    }
+}
